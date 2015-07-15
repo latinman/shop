df67cdc9e72a8ad8040e8aa14e7347bf463c89b7
Ernesto Rivas <erivas78@gmail.com>
2015-07-15 15:03:36 -0400

    Added migration for stripe_customer_token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714165842) do
+ActiveRecord::Schema.define(version: 20150715184531) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150714165842) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
