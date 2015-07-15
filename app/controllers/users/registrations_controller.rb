b888be7b3868af603227784ae7e5b10882e4e9a5
Ernesto Rivas <erivas78@gmail.com>
2015-07-15 15:43:50 -0400

    Added custom css for flash notice & added user plan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
-# class Users::RegistrationsController < Devise::RegistrationsController
-#
+ class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :select_plan, only: :new
+
 #   def create
 #     super do |resource|
 #       if params[:plan]
@@ -8,4 +9,13 @@
 #       end
 #     end
 #   end
-# end
+
+   private
+
+   def select_plan
+     unless params[:plan] && params[:plan] == "3"
+       flash[:notice] = "Please sign up before making purchases. Thank you!"
+       redirect_to root_url
+     end
+   end
+  end
