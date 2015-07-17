27fc16e724b3f5242298d2261a42ca8f6b039921
Ernesto Rivas <erivas78@gmail.com>
2015-07-16 21:50:27 -0400

    Added Users controller, views & able to create profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,30 @@ class ProfilesController < ApplicationController
 
   def new
     @user = User.find(params[:user_id])
-    @profile = @user.build_profile
+    @profile = Profile.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+
+    if @profile.save
+      flash[:success] = "Profile Created!"
+      redirect_to user_path(params[:user_id])
+    else
+      redirect_to "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :phone_number, :city, :country)
   end
 
 end
