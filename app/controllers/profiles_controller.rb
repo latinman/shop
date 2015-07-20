637bcfd92a298a8da6d98757edaa3ab1dd1a2474
Ernesto Rivas <erivas78@gmail.com>
2015-07-20 12:30:57 -0400

    Added cc fields for edit form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
 
   def new
     @user = User.find(params[:user_id])
@@ -22,11 +24,27 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
 
+  def update
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to users_path(params[:user_id])
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :phone_number, :city, :country, :contact_email)
+    params.require(:profile).permit(:first_name, :last_name, :phone_number, :city, :country, :contact_email, :card_number, :card_code, :card_month)
   end
 
+  def only_current_user
+    @user = User.find(params[:user_id])
+    redirect_to root_url unless @user == current_user
+  end
 end
 
